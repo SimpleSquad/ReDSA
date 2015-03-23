e73baeaa550f32b70070d80508fc96ac9f2f870c
gayathree <gayathreemunasinghe@gmail.com>
2015-03-23 18:59:59 +0530

    Added a comment
    Added the details about the use of the class

--- ListPkg/ListHouse.java
+++ ListPkg/ListHouse.java
@@ -1,5 +1,8 @@
 package ListPkg;
 
-
+//list house class is used to implement the listable interface,
+//Here the implementation of abstract methords are done . and this act
+//as the concrete class of the Listable Interface
 public class ListHouse implements Listable{
+  
 }

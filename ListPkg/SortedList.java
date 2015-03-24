b9c07f2b28ec402d1b6e367c1684f786a3e24a14
NethmiH <nethmih29@gmail.com>
2015-03-24 21:21:30 +0530

    Added Code to Sorted List
    ADT methods are added

--- ListPkg/SortedList.java
+++ ListPkg/SortedList.java
@@ -20,6 +20,60 @@ public class SortedList {
 		  starter = null;
 		  currentPos = null;
 	  }
+//////////////////// ADT Methords ////////////////////////////////////////////
 
+//	  this is the methord we use to insert items in to the linklist
+
+	  public void insert (ListHouse item) 
+		 {
+		    SortedList newNode = new SortedList(); // Reference to node being inserted
+		    SortedList backer = new SortedList(); //  reference to the back one
+		    SortedList pos = new SortedList(); //  reference to a position
+		     
+		      pos = starter;
+			  backer = null;
+			  boolean moreToSearch = (pos != null);//this is used to find whether all the nodes are examined
+
+		    while (moreToSearch) 
+		    {
+		    	switch (item.compareTo(pos.house)) {
+				case "Less":
+//					 if compare result is less than zero that means we have found the perfect place 
+//					 for this node. otherwords our lotnumber is smaller than current comparing nodes lot number
+//					 we are inserting in a sorted order, ascending order .
+					 moreToSearch = false;
+					break;
+
+				default:
+//					If the node number is larger we should move the location to a higher node
+//					and the location should be the previous location
+					backer = pos;
+					pos = pos.next;
+//					then check whether we are at the end of the list
+					moreToSearch = (pos != null);
+					break;
+				}		    	
+
+		    }
+//             Inserting begins
+			  newNode.house = (ListHouse)item.copy();
+//			if the previous node is null the current position is the first node
+			  if (backer == null)
+//		    so we enter the first element to the next position 
+//			and the new node to the starter position				  
+			  {
+			  newNode.next = starter;
+			  starter = newNode;
+			  }
+			  else
+//				  or we make the current position next and newnode the next of the previous
+			  {
+			  newNode.next = pos;
+	  		  backer.next = newNode;
+			  }
+//			  increase no of items
+			  numItems++;
+
+		  }
 
 }
